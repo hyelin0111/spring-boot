15183446741f87e3bc63ce1fe22dd6f3d37fdc42
김혜린 <sjgpfls@gmail.com>
2022-02-26 18:02:17 +0900

    Initial commit

---
+++ src/test/java/kvp/hyelin/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kvp.hyelin.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
