20b815b28d4621b7b06cee8fcf12bbe32317d185
김혜린 <sjgpfls@gmail.com>
2022-02-26 23:43:07 +0900

    fix: AutoConfiguration 수정

--- spring-boot-web/src/main/java/kvp/hyelin/springboot/Application.java
+++ spring-boot-web/src/main/java/kvp/hyelin/springboot/Application.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication(SpringBootApplication.class);
+		SpringApplication application = new SpringApplication(Application.class);
 		application.setWebApplicationType(WebApplicationType.NONE);
 		application.run(args);
 	}
