1f4368bc140004faa75438e20c0e23231d12a6af
김혜린 <sjgpfls@gmail.com>
2022-03-05 21:41:05 +0900

    feat: Undertow 어플리케이션 추가

---
+++ spring-boot-web-undertow/src/main/java/kvp/hyelin/springboot/Application.java
@@ -0,0 +1,39 @@
+package kvp.hyelin.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+// import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+// import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// import org.apache.catalina.connector.Connector;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello Spring";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	// @Bean
+	// public ServletWebServerFactory serverFactory() {
+	// 	TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+	// 	tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+	// 	return tomcat;
+	// }
+	//
+	// private Connector createStandardConnector() {
+	// 	Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+	// 	connector.setPort(8081);
+	// 	return connector;
+	// }
+
+}
