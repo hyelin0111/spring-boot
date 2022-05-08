5195f9230454feb61c15a7f27bce19096357e40d
김혜린 <sjgpfls@gmail.com>
2022-05-08 20:47:22 +0900

    feat: actuator spring-boot-admin

---
+++ spring-boot-actuator-monitor/src/main/java/kvp/hyelin/springbootactuatormonitor/Application.java
@@ -0,0 +1,15 @@
+package kvp.hyelin.springbootactuatormonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
